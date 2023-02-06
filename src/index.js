d3a9d27d6385cdafcc859bb0705bb2a652a31a10
Сергей Козачук <panteon@MacBook-Pro-Sergej.local>
2023-02-06 13:23:04 +0300

    build react webpack

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./main.scss";
+
+const App = () => <p>Hello</p>;
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<App />);
